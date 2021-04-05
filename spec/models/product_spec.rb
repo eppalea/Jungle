a62237efae427b8305d64951b6c0935d181ffe92
Eppa Lea Turniawan <eppalea@gmail.com>
2021-04-05 23:17:11 +0000

    create five validation tests for the creation of a product

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe 'Validations' do
+    it 'creates a product' do
+      @category = Category.create(name: "games")
+      @product = Product.create(name: "Gameboy", price: 10000, quantity: 5, category: @category)
+      puts @product.errors.full_messages
+    end
+
+    it 'contains a name' do
+      @category = Category.create(name: "games")
+      @product = Product.create(name: nil, price: 100 , quantity: 5, category: @category)
+      puts @product.errors.full_messages
+    end
+
+    it 'contains a price' do
+      @category = Category.create(name: "games")
+      @product = Product.create(name: "Gameboy", price: nil , quantity: 5, category: @category)
+      puts @product.errors.full_messages
+    end
+
+    it 'contains a quantity' do
+      @category = Category.create(name: "games")
+      @product = Product.create(name: "Gameboy", price: 100 , quantity: nil, category: @category)
+      puts @product.errors.full_messages
+    end
+
+    it 'contains a category' do
+      @category = Category.create(name: "games")
+      @product = Product.create(name: "Gameboy", price: nil , quantity: 5, category: nil)
+      puts @product.errors.full_messages
+    end
+
+  end
 end
