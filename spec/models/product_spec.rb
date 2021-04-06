d8b14c3a4de9dc256afb5e4583f90b2025597eba
Eppa Lea Turniawan <eppalea@gmail.com>
2021-04-06 01:55:20 +0000

    add except statments to each Validation

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,31 +6,31 @@ RSpec.describe Product, type: :model do
     it 'creates a product' do
       @category = Category.create(name: "games")
       @product = Product.create(name: "Gameboy", price: 10000, quantity: 5, category: @category)
-      puts @product.errors.full_messages
+      expect(@product).to be_valid
     end
 
     it 'contains a name' do
       @category = Category.create(name: "games")
       @product = Product.create(name: nil, price: 100 , quantity: 5, category: @category)
-      puts @product.errors.full_messages
+      expect(@product.errors.full_messages).to include("Name can't be blank")
     end
 
     it 'contains a price' do
       @category = Category.create(name: "games")
       @product = Product.create(name: "Gameboy", price: nil , quantity: 5, category: @category)
-      puts @product.errors.full_messages
+      expect(@product.errors.full_messages).to include("Price can't be blank")
     end
 
     it 'contains a quantity' do
       @category = Category.create(name: "games")
       @product = Product.create(name: "Gameboy", price: 100 , quantity: nil, category: @category)
-      puts @product.errors.full_messages
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
     end
 
     it 'contains a category' do
       @category = Category.create(name: "games")
       @product = Product.create(name: "Gameboy", price: nil , quantity: 5, category: nil)
-      puts @product.errors.full_messages
+      expect(@product.errors.full_messages).to include("Category can't be blank")
     end
 
   end
