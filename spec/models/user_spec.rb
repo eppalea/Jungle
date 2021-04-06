fa0e40d438a1460d42bafd86a2c435b54e7217a0
Eppa Lea Turniawan <eppalea@gmail.com>
2021-04-06 01:56:13 +0000

    update user table to specify first_name and last_name instead of only name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe 'Validations' do
+    it 'creates a user' do
+      @user = User.new(first_name: "EppaLea", last_name: "Turniawan", email: "eppalea@nomail.com", password: "1234", password_confirmation: "1234")
+      @user.valid?
+      puts @user.errors.full_messages
+      expect()
+    end
+      
+    # it 'emails must be unique' do
+    #  expect(@user.email.downcase).to be == email.downcase
+    # end
+  end  
 end
